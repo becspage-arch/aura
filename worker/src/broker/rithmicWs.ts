d93daabe1149d22cc8f281d6504b83f550599cc1
gathereducation-team <rebecca@gather.education>
2026-01-15 11:33:08 +0000

    feat(worker): log inbound rithmic websocket frames

--- worker/src/broker/rithmicWs.ts
+++ worker/src/broker/rithmicWs.ts
@@ -3,9 +3,24 @@ import WebSocket from "ws";
 export function createRithmicWs(url: string): WebSocket {
   const ws = new WebSocket(url);
 
-  ws.on("open", () => console.log("[rithmic-ws] open"));
-  ws.on("error", (e) => console.error("[rithmic-ws] error", e));
-  ws.on("close", () => console.log("[rithmic-ws] close"));
+  ws.on("open", () => {
+    console.log("[rithmic-ws] open");
+  });
+
+  ws.on("message", (data) => {
+    console.log("[rithmic-ws] message", {
+      type: typeof data,
+      length: Buffer.isBuffer(data) ? data.length : undefined,
+    });
+  });
+
+  ws.on("error", (e) => {
+    console.error("[rithmic-ws] error", e);
+  });
+
+  ws.on("close", () => {
+    console.log("[rithmic-ws] close");
+  });
 
   return ws;
 }
