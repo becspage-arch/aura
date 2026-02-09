1f8efde0190847a6dea059970a1d1447aa704d38
gathereducation-team <rebecca@gather.education>
2026-02-09 12:02:39 +0000

    Fix

--- src/components/live-control/LiveControlSwitches.tsx
+++ src/components/live-control/LiveControlSwitches.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { ManualOrderButton } from "@/src/app/components/ManualOrderButton";
+import { ManualOrderButton } from "@/components/ManualOrderButton";
 
 
 type PauseGetResponse = {
