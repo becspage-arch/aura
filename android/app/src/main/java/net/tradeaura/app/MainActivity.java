1001f4f643863f186d2ccf8bdc133e6597eb4a48
gathereducation-team <rebecca@gather.education>
2026-02-11 13:05:10 +0000

    Update

--- android/app/src/main/java/net/tradeaura/app/MainActivity.java
+++ android/app/src/main/java/net/tradeaura/app/MainActivity.java
@@ -1,16 +1,5 @@
 package net.tradeaura.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-// OneSignal Cordova plugin
-import com.onesignal.cordova.OneSignalPush;
-
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    registerPlugin(OneSignalPush.class);
-  }
-}
+public class MainActivity extends BridgeActivity {}
